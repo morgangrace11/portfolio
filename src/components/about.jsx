83551614e1200348b26f55b4ae6016a347338746
Morgan Grace <morgangrace@Morgans-iMac.local>
2021-03-08 17:56:52 -0500

    Redoing portfolio, Adding new image frame that will contain a picture in the future, Added about me paragraph that's more in depth and personal.

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -3,7 +3,22 @@ import Typing from 'react-typing-animation';
 
 const About = () => (
   <div className="aboutContent">
-    <Typing>
+    <div>Hello! My name is Morgan Grace!</div>
+      <div style={{width: "220px",
+        height: "220px",
+        marginRight: "auto",
+        backgroundColor: "#f0f0f0",
+        marginLeft: "auto",
+        boxShadow: "inset 0px 0px 11px grey",
+        borderRadius: "7px",
+        marginTop: "20px",
+        marginBottom: "20px"}}> 
+      <img width="200" height="200" style={{marginTop: "10px"}}></img>
+    </div>
+    <div>
+      I am a software engineer with a passion for front end developing. I got into software engineering as a career 2 years ago and went to Hack Reactor to further my skills. During my time at Hack Reactor I learned the entire software development lifecycle while also learning to work in a team and collaborate effectively with others in a remote enviroment. I then moved to a fulltime position at Bluewater Reporting where I worked with the fullstack and got to learn new things like handlebars.js and working with page.js to MSSQL on the database side. I also had the oppurtunity to help introduce new things like the use of github for version control.
+    </div>
+    {/* <Typing>
       <br />
       <span>
         Morgans-iMac:~ morgangrace$ npm install morgan
@@ -28,7 +43,7 @@ const About = () => (
       <span>
         npm <span className='npmWarn'>WARN</span>: caffiene required
     </span>
-    </Typing>
+    </Typing> */}
   </div>
 )
 
